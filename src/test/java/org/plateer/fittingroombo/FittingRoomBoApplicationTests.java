de5548288057c0a20fa6db8008a13dfe8540648c
Seunghyeon Jung <armyso9506@plateer.com>
2023-02-08 16:15:16 +0900

    init 초기설정

---
+++ src/test/java/org/plateer/fittingroombo/FittingRoomBoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.plateer.fittingroombo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittingRoomBoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
